ed703015b0d8fd677c37ee900f9207ed5ced0fe8
Xiaosong Chen <chen873374255@gmail.com>
2019-01-30 15:20:39 -0500

    hw1 try4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
   title = 'jiuzhangAngular';
   changeableName = 'myName';
-=======
-  title = '九章全栈基础课!';
->>>>>>> 161022dac8266ac7990b7f174e5823bee3573375
+
 }
